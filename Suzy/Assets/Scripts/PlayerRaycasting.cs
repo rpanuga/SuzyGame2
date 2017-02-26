03b40354a175bc5466de0d0355cfa0aa47b60caf
Ro Pan <rohpanu@hotmail.com>
2017-02-26 16:48:10 -0500

    Initial Git of new repo

---
+++ Suzy/Assets/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour {
+
+	public float distanceToSee;
+
+	public RaycastHit whatIHit;
+
+	private bool didIHit;
+
+	void Start () {
+		didIHit = false;
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 forward = transform.TransformDirection (Vector3.forward) * distanceToSee;
+
+		//Debug.DrawRay (this.transform.position, (this.transform.forward * distanceToSee), Color.red);
+		Debug.DrawRay(transform.position,forward, Color.green);
+
+		//DEBUGGING TOOL
+		if (Physics.Raycast(transform.position, forward, out whatIHit)) {
+			//Debug.Log("I touched " + whatIHit.collider.gameObject.name + " from " + whatIHit.distance);
+			didIHit = true;
+		} 
+
+
+	}
+	public bool didIHitAnything() {
+		return didIHit;
+	}
+}
---
+++ Suzy/Assets/Scripts/VaseTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaseTrigger : MonoBehaviour {
+
+	public GameObject vase;
+	public float speed;
+
+	// Update is called once per frame
+	void Start () {
+		vase.GetComponent<Rigidbody> ().velocity = vase.transform.forward * speed;
+		vase.GetComponent<Rigidbody> ().AddForce (transform.forward * speed, ForceMode.Impulse);
+	}
+		
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			vase.gameObject.SetActive (true);
+
+			// TODO: Destroy the vase after xxx seconds OR CHANGE IT INTO BROKEN CLASS
+			Destroy(vase, 4.0f); 
+			Destroy (this.gameObject, 4.0f);
+
+		}
+	}
+
+}
