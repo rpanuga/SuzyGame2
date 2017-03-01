e735505942485637396e4ee3ba16e211203daef6
Ro Pan <rohpanu@hotmail.com>
2017-03-01 15:22:25 -0500

    Added Window Stranger basic coding and Nav

---
+++ Suzy/Assets/Scripts/AI/StrangerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+public class StrangerMove : MonoBehaviour {
+
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void StartMoving() {
+		this.GetComponent<NavMeshAgent>().destination = goal.position;
+	}
+}
